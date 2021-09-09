0c9f24f474ad3594f9274c1ddc02ba0ad75cfe60
wooseok0727 <crash2@naver.com>
2021-09-09 14:13:28 +0900

    9월 9일

--- react-firebase-bbs-01/src/App.js
+++ react-firebase-bbs-01/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import { BBsMain, Footer, Header, MainNav } from "./comps";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BBsMain, BBsWrite, Footer, Header, MainNav } from "./comps";
 
 const App = () => {
   return (
@@ -9,7 +9,8 @@ const App = () => {
         <Header />
         <MainNav />
         <section className="main_section">
-          <BBsMain />
+          <Route exact path="/" component={BBsMain} />
+          <Route exact path="/write" component={BBsWrite} />
         </section>
         <Footer />
       </div>
--- react-firebase-bbs-01/src/comps/index.jsx
+++ react-firebase-bbs-01/src/comps/index.jsx
@@ -3,3 +3,4 @@ export { default as MainNav } from "./MainNav";
 export { default as BBsMain } from "./BBsMain";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
+export { default as BBsWrite } from "./BBsWrite";
