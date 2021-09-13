6000acc2717e35e8f4254723dacba5d00d7e4842
wooseok0727 <crash2@naver.com>
2021-09-13 13:37:10 +0900

    9월 13일

--- react-bucket-01/src/comps/BucketMain.jsx
+++ react-bucket-01/src/comps/BucketMain.jsx
@@ -1,46 +1,66 @@
 import React, { useState } from "react";
 import BuckList from "./BuckList";
 import BuckInput from "./BuckInput";
+import uuid from "react-uuid";
+import moment from "moment";
 
 function BucketMain() {
   const [buckList, setBuckList] = useState([
-    {
-      b_id: 0,
-      b_flag: 0,
-      b_start_Date: "2021-09-13",
-      b_title: "리액트 정복",
-      b_end_date: "",
-      b_end_check: false,
-      b_cancel: false,
-    },
-    {
-      b_id: 1,
-      b_flag: 0,
-      b_start_Date: "2021-09-13",
-      b_title: "추석맞이",
-      b_end_date: "",
-      b_end_check: false,
-      b_cancel: false,
-    },
+    // {
+    //   b_id: 0,
+    //   b_flag: 0,
+    //   b_start_Date: "2021-09-13",
+    //   b_title: "리액트 정복",
+    //   b_end_date: "",
+    //   b_end_check: false,
+    //   b_cancel: false,
+    // },
+    // {
+    //   b_id: 1,
+    //   b_flag: 0,
+    //   b_start_Date: "2021-09-13",
+    //   b_title: "추석맞이",
+    //   b_end_date: "",
+    //   b_end_check: false,
+    //   b_cancel: false,
+    // },
   ]);
 
+  const buck_insert = (bucket_text) => {
+    const bucket = {
+      b_id: uuid(),
+      b_start_date: moment().format("YYYY[-]MM[-]DD HH:mm:ss"),
+      b_title: bucket_text,
+      b_flag: 0,
+      b_end_date: "",
+      b_end_check: false,
+      b_cancel: false,
+    };
+    // 원래 있던 bucketList 에 bucket을 추가하기
+    setBuckList([...buckList, bucket]);
+  };
+
   const flag_change = (id) => {
-    const result = buckList.map((bucket) => {
+    const _bucketList = buckList.map((bucket) => {
+      /**
+       * 전달받은 id 와 같은 항목의 flag를 1 증가시키기
+       */
       if (bucket.b_id === id) {
         return {
           ...bucket,
-          b_flag: bucket.b_flag >= 3 ? 0 : bucket.b_flag + 1,
+          b_flag: bucket.b_flag + 1,
         };
       } else {
         return bucket;
       }
     });
-    setBuckList([...result]);
+    // 원래의 bucketList를 _bucklist와 교체
+    setBuckList([..._bucketList]);
   };
 
   return (
     <div className="w3-container-fluid">
-      <BuckInput />
+      <BuckInput buck_insert={buck_insert} />
       {/* BuckList 컴포넌트에 buckList 상태(변수) 전달하기 */}
       <BuckList buckList={buckList} flag_change={flag_change} />
     </div>
