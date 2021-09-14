00f945439064dd76288c2640c3420b10ab899f61
wooseok0727 <crash2@naver.com>
2021-09-14 16:00:12 +0900

    9월 14일

--- react-nodejs-api/app.js
+++ react-nodejs-api/app.js
@@ -3,6 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+/**
+ * Cross-Origin Resource Sharing
+ * 서로다른 서버간에 데이터를 주고받을 때 보안문제로 인해
+ * 발생할 수 있는 ISSUE
+ *
+ * XSS 공격
+ */
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -28,7 +36,18 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-app.use(cors());
+
+// CORS를 허용할 Origin List
+const whiteList = ["http://localhost:5000", "http://localhost:3000"];
+const corsOption = {
+  origin: (origin, callback) => {
+    const isWhiteList = whiteList.indexOf(origin) !== -1;
+    callback(null, isWhiteList);
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOption));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
